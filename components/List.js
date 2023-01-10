ed8b94fcc0c68fc1d6d1875993540b225a6e3f48
Giao Ngo <giao.ngo@metropolia.fi>
2023-01-10 11:22:31 +0200

    complete fetching data with AJAX task A

--- components/List.js
+++ components/List.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {FlatList} from 'react-native';
 import ListItem from './ListItem';
 
-// eslint-disable-next-line react/prop-types
-const List = ({list}) => {
+const List = () => {
+  const url =
+    'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+  const [media, setMedia] = useState([]);
+  const loadMedia = async () => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      return json;
+    } catch (e) {
+      console.log('error', e);
+    }
+  };
+  useEffect(() => {
+    loadMedia().then((data) => {
+      setMedia(data);
+    });
+  }, []);
+
   return (
     <FlatList
       style={{flex: 1, flexDirection: 'column'}}
-      data={list}
+      data={media}
+      keyExtractor={(item, index) => index.toString()}
       renderItem={({item}) => <ListItem singleMedia={item} />}
     />
   );
