79c60fe3b4e68abb2a040a12cfa403b66b58ab13
Giao Ngo <giao.ngo@metropolia.fi>
2023-01-16 09:56:59 +0200

    dummy login/logout buttons

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import {StatusBar} from 'react-native';
+import {MainProvider} from './contexts/MainContext';
 import Navigator from './navigators/Navigator';
 
 const App = () => {
   return (
-    <>
+    <MainProvider>
       <Navigator></Navigator>
       <StatusBar backgroundColor={'#ff3300'} />
-    </>
+    </MainProvider>
   );
 };
 
--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,11 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useContext} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../views/Home';
 import Profile from '../views/Profile';
 import Single from '../views/Single';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import Login from '../views/Login';
+import {MainContext} from '../contexts/MainContext';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -34,14 +36,21 @@ const TabScreen = () => {
 };
 
 const StackScreen = () => {
+  const {isLoggedIn} = useContext(MainContext);
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="Tabs"
-        component={TabScreen}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen name="Single" component={Single} />
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen
+            name="Tabs"
+            component={TabScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen name="Single" component={Single} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={Login} />
+      )}
     </Stack.Navigator>
   );
 };
