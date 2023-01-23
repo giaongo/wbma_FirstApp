0698b8c4888c185bf146356bdfb12eb17627b8bd
Giao Ngo <giao.ngo@metropolia.fi>
2023-01-23 09:54:45 +0200

    complete task A

--- views/Profile.js
+++ views/Profile.js
@@ -1,13 +1,32 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, {useContext} from 'react';
-import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {StyleSheet, SafeAreaView, Text, Button, Image} from 'react-native';
 import {MainContext} from '../contexts/MainContext';
+import {useTag} from '../hooks/ApiHooks';
+import {uploadsUrl} from '../utils/variables';
 
 const Profile = () => {
+  const {getFilesByTag} = useTag();
   const {setIsLoggedIn, user, setUser} = useContext(MainContext);
+  const [avatar, setAvatar] = useState('');
+
+  const loadAvatar = async () => {
+    try {
+      const avatarArray = await getFilesByTag('avatar_' + '2689');
+      setAvatar(avatarArray.pop());
+    } catch (error) {
+      console.log('user avatar fetch failed', error.messsage);
+    }
+  };
+
+  useEffect(() => {
+    loadAvatar();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
+      <Image source={{uri: uploadsUrl + avatar}} style={styles.image} />
       <Text>username: {user.username}</Text>
       <Text>email: {user.email}</Text>
       <Text>fullname: {user.full_name}</Text>
@@ -35,6 +54,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 40,
+  },
+  image: {
+    width: 200,
+    height: 300,
   },
 });
 export default Profile;
